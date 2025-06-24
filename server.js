4f76c166413145ab1953ce059706101b919ce6f6
dheeraj231507 <dheeraj.codeup@gmail.com>
2025-06-25 00:49:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://rippleshotaifront.vercel.app"],
     credentials: true,
   })
 );
