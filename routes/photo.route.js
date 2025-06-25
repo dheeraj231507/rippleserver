2851b879a827fe1e524a30695fddee4b6e96ba62
dheeraj231507 <dheeraj.codeup@gmail.com>
2025-06-25 12:29:44 +0530

    Update photo.route.js

--- routes/photo.route.js
+++ routes/photo.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   upload,
   analyzePhoto,
-} from "../controllers/analysisphoto.controller.js";
+} from "../controllers/analysisPhoto.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
